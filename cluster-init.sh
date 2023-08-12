a43db92eab0b9d71dd98cf8fb39dcdd84b330b4e
brahim <brahim.hamdi.consult@gmail.com>
2023-08-12 15:26:23 +0200

    add labs file

--- cluster-init.sh
+++ cluster-init.sh
@@ -4,7 +4,5 @@ vagrant ssh -c 'mkdir -p $HOME/.kube' kube-control-plane
 vagrant ssh -c 'sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config' kube-control-plane
 vagrant ssh -c 'sudo chown $(id -u):$(id -g) $HOME/.kube/config' kube-control-plane
 # vagrant ssh -c 'kubectl apply -f weave-daemonset-k8s.yaml' kube-control-plane
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
-
-
+vagrant ssh -c 'kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml' kube-control-plane
 
