42ced1014471eecd2b82dc233e15974292348b27
brahim <brahim.hamdi.consult@gmail.com>
2023-09-15 09:32:52 +0200

    add Labs

--- .install/4-kubeconfig.sh
+++ .install/4-kubeconfig.sh
@@ -4,5 +4,5 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-scp -i .vagrant/machines/kube-control-plane/virtualbox/private_key -r /home/vagrant/.kube $HOME/
+scp -o StrictHostKeyChecking=no -i ../.vagrant/machines/kube-control-plane/virtualbox/private_key -r vagrant@192.168.56.10:/home/vagrant/.kube $HOME/
 
