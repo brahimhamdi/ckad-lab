d516a0bb755f2b1a4523478b7919eeaed9ac6972
brahim <brahim.hamdi.consult@gmail.com>
2023-10-04 14:52:36 +0200

    4-kubeconfig.sh : sudo mkdir /etc/apt/keyrings

--- .install/4-kubeconfig.sh
+++ .install/4-kubeconfig.sh
@@ -1,3 +1,4 @@
+sudo mkdir /etc/apt/keyrings
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
