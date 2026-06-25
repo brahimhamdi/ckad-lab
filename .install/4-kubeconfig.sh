e4cfea8fd9e079d0731fa8818932845429883dbc
Brahim Hamdi <brahim.hamdi@gmail.com>
2026-06-25 20:02:56 +0200

    modifs ...

--- .install/4-kubeconfig.sh
+++
@@ -1,9 +0,0 @@
-sudo mkdir /etc/apt/keyrings
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-scp -o StrictHostKeyChecking=no -i ../.vagrant/machines/kube-control-plane/virtualbox/private_key -r vagrant@192.168.56.10:/home/vagrant/.kube $HOME/
-
